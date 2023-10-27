d316fc213918a70f1c20d112207fc66b8fae9bbf
markup54 <marzenakupka@tlen.pl>
2023-10-27 08:31:28 +0200

    bindowanie widoków

--- app/src/main/java/pl/zabrze/zs10/koszykowka3ppiatek/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/koszykowka3ppiatek/MainActivity.java
@@ -3,12 +3,47 @@ package pl.zabrze.zs10.koszykowka3ppiatek;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import pl.zabrze.zs10.koszykowka3ppiatek.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    private int punkty = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.textView.setText(String.valueOf(punkty));
+        binding.button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty++;
+                        binding.textView.setText(String.valueOf(punkty));
+                    }
+                }
+        );
+        binding.button2.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty+=2;
+                        binding.textView.setText(String.valueOf(punkty));
+                    }
+                }
+        );
+        binding.button3.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty+=3;
+                        binding.textView.setText(String.valueOf(punkty));
+                    }
+                }
+        );
     }
 }
